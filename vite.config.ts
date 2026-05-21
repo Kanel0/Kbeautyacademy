6e3c24da22a438de6d7c6506d3457ef2dc5159f5
Kanel04 <jaosoadongaanelka@gmail.com>
2026-05-21 10:53:56 +0300

    Add GitHub Actions CI/CD and clean dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: "/Kbeautyacademy/",
   plugins: [react(), tailwindcss()],
 })
